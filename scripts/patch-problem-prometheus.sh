b56581f70f169363c085c25a6b5f76f35d4753fc
Pham Tan Tai <taipham.dev@gmail.com>
2025-05-16 13:41:05 +0700

    feat: add script to patch Prometheus deployment

---
+++ scripts/patch-problem-prometheus.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Error: UPGRADE FAILED: cannot patch "prometheus-server" with kind PersistentVolumeClaim: persistentvolumeclaims "prometheus-server" 
+#is forbidden: only dynamically provisioned pvc can be resized and the storageclass that provisions the pvc must support resize && 
+#cannot patch "prometheus-alertmanager" with kind StatefulSet: StatefulSet.apps "prometheus-alertmanager" is invalid: spec: Forbidden: 
+#updates to statefulset spec for fields other than 'replicas', 'ordinals', 'template', 'updateStrategy', 'persistentVolumeClaimRetentionPolicy' and 'minReadySeconds' are forbidden
+
+echo "--------------Xóa Prometheus và Alertmanager deployments để giải phóng PVC------------------"
+sudo kubectl delete deploy -n prometheus prometheus-server
+sudo kubectl delete statefulset -n prometheus prometheus-alertmanager
+
+echo "--------------Xóa PVC cũ (mất dữ liệu metrics cũ!)------------------------------------------"
+sudo kubectl delete pvc -n prometheus prometheus-server
+sudo kubectl delete pvc -n prometheus prometheus-alertmanager-prometheus-alertmanager-0
+
+echo "Upgrade lại Helm chart với prometheus-values.yaml"
+helm upgrade prometheus prometheus-community/prometheus -n prometheus -f prometheus-values.yaml
+
+echo "-----------patch deployment------------"
+sudo kubectl -n prometheus patch deployment prometheus-server --type=strategic -p '{
+  "spec": {
+    "template": {
+      "spec": {
+        "containers": [{
+          "name": "prometheus-server",
+          "livenessProbe": {
+            "httpGet": {"path":"/prometheus/-/healthy","port":9090},
+            "initialDelaySeconds":30,
+            "timeoutSeconds":5,
+            "failureThreshold":6
+          },
+          "readinessProbe": {
+            "httpGet": {"path":"/prometheus/-/ready","port":9090},
+            "initialDelaySeconds":30,
+            "timeoutSeconds":5,
+            "failureThreshold":6
+          }
+        }]
+      }
+    }
+  }
+}'
